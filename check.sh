58c525bb041e9a1a1cc870ae9facf6697fe4a54a
lesda <99568565+lesliedaina@users.noreply.github.com>
2022-12-26 11:14:35 -0600

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+USER=${user}
+cat permission.txt | grep -i $USER 
+
+if
+[[ $? -eq 0]]
+then
+echo "you have permission to run this job"
+ 
+ else
+ echo "you don't have permission to run this job"
+ exit 1
+ fi
+EOF
